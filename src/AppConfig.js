b9ab1df41b63ddd0b67a2c62e2b2543cee85573e
consuti-tec <consuti@consuti.com.br>
2023-04-18 00:46:07 -0300

    first commit

--- src/AppConfig.js
+++ src/AppConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-  ColorTheme: "#FC6405",
-  UrlPortal: "https://escoladoesporte.com.br/redesporti/unidades/estoril-esportes",
+  ColorTheme: "#FF7227",
+  UrlPortal: "https://escoladoesporte.com.br/redesporti/unidades/associacao-esportiva-e-cultural-cnb",
   UrlPainel: "https://painel.escoladoesporte.com.br/",
   TimeSplashScreen: 3000,
 };
